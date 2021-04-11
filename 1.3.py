55f3eaf64034a36e86eca3780d772e5f426536f7
PranjalKushwaha <200050107@iitb.ac.in>
2021-04-11 20:20:18 +0530

    Checkpoint week 1

--- 1.3.py
+++ 1.3.py
@@ -7,9 +7,7 @@ width = int(original.shape[1] * scale_percent / 100)
 height = int(original.shape[0] * scale_percent / 100)
 dim = (width, height)
 original = cv.resize(original, dim, None, fx = 10,fy=10)
-blue = original.copy()
-blue[:,:,2]=0
-blue[0,:,:]=255
+blue = cv.cvtColor(original, cv.COLOR_BGR2RGB)
 cv.imshow("Original", original)
 cv.imshow("Blue", blue)
 cv.waitKey(0)
