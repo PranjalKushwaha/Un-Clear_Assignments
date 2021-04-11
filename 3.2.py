453f7c401afd09612d1c47ba0d5c75602f04eee3
PranjalKushwaha <200050107@iitb.ac.in>
2021-04-11 20:18:18 +0530

    Checkpoint Week 1

--- 3.2.py
+++ 3.2.py
@@ -11,17 +11,8 @@ while True:
     dim = (width, height)
     frame = cv.resize(frame, dim, None, fx = 10,fy=10)
     frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    #ret,frame = cv.threshold(frame,120,255,cv.THRESH_TRUNC )
-    #frame = cv.medianBlur(frame,5)
-    #ret,frame = cv.threshold(frame,127,255,cv.THRESH_BINARY)
-    
     
     frame = cv.adaptiveThreshold(frame, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY,11,1)
-    frame = cv.GaussianBlur(frame,(5,5),0)
-    kernel = np.ones((5,5),np.uint8)
-    #frame = cv.morphologyEx(frame, cv.MORPH_CLOSE, kernel)
-    #frame = cv.dilate(frame,kernel,iterations = 1)
-    #frame = cv.erode(frame,kernel,iterations = 1)
     cv.imshow('frame', frame)
     if cv.waitKey(1) == ord('a'):
         break
